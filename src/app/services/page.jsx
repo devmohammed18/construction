b27856bd4229540899ec4d9c5e888875b5d9a812
devmohammed18 <devmohammed18@gmail.com>
2024-09-24 10:59:25 -0600

    update page services

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -25,25 +25,27 @@ function Services() {
 
       
 
-          
+        
             {inforServices.map((item,index)=>(
-               <div className=' sm:flex-col-reverse md:flex-col-reverse  h-full border-0 border-solid border-red-900 flex justify-center items-center space-y-4 '>
-                  <div key={index} className='sm:h-full  sm:w-full md:h-full md:w-full w-1/2 h-full  border-0 border-solid border-white flex flex-col justify-start items-start opacity-85 hover:opacity-100' >
-                      <div className=' w-full flex justify-between items-end border border-solid border-white'>
+               <div key={index} className=' sm:flex-col-reverse md:flex-col-reverse  h-full border-0 border-solid border-red-900 flex justify-center items-center space-y-4 '>
+                  <div key={index} className='sm:h-full sm:mt-3 md:mt-3 sm:w-full md:h-full md:w-full w-1/2 h-full  border-0 border-solid border-white flex flex-col justify-start items-start opacity-85 hover:opacity-100 ' >
+                      <div className=' w-full flex justify-between items-end border border-solid border-white' onClick={()=>console.log('Diiiiiv===')}>
                            <h1 className='mx-4  text-2xl text-white uppercase border-b-2 border-b-solid border-b-red-800 ' > {item.title}</h1><br />
-                           {index===show && !toggle?<span className='text-white text-2xl mx-4 '>
-                           <IoIosArrowUp onClick={()=>{setToggle(false), console.log('Up------')}} /> </span> 
-                           :<span className='text-white text-2xl mx-4 ' ><IoIosArrowDown onClick={()=>{setToggle(true),setShow(index),console.log('down------')}} /></span>
+                           
+                           {index===show && toggle?
+                           <span className='text-white text-2xl mx-4 w-8 h-8 bg-yellow-500 flex justify-center items-center rounded-full '  onClick={()=>{setToggle(false), console.log('Up------')}} ><IoIosArrowUp/> </span> 
+                           :<span className='text-white text-2xl mx-4 w-8 h-8 bg-yellow-500 flex justify-center items-center rounded-full ' onClick={()=>{setToggle(true),setShow(index),console.log('down------')}}><IoIosArrowDown o /></span>
                            }
                      </div>
                      
-                    {index===show && toggle? <p className=' text-xl text-white leading-10 px-4 text-justify' >{item.desc}</p>:''}
+                     {index===show && toggle?<p className=' text-xl text-white border px-4 text-justify' >{item.desc}</p>:''}
                   </div>
-                  <div className='sm:w-full sm:m-0 md:w-full md:m-0 w-1/2 h-80  text-white  border border-solid border-red-900 mr-4'>
+                  <div className='sm:w-full  sm:m-0 md:w-full md:m-0 w-1/2 h-80 box-content  text-white  border-0 border-solid border-red-900 mr-4'>
                      <SlideServices images={item.urls}/>
                   </div>
                </div>
             ))}
+
     </div>
   )
 }
